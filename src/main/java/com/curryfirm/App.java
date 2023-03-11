2ffd7f7055a6c8310c10dced047ff424dd302026
bencurry0 <18700143+bencurry0@users.noreply.github.com>
2023-03-11 12:33:25 -0500

    Initial commit

---
+++ src/main/java/com/curryfirm/App.java
@@ -0,0 +1,14 @@
+package com.curryfirm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        processArgs();
+        Drawing drawing = new Drawing();
+    }
+}
